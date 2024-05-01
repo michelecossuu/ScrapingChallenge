f29aa5b73d7a2abd5bd0f3b26ed4e66065f9f01f
mcossu <mcossu@intesigroup.com>
2024-05-01 11:55:49 +0200

    Add db connection

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,8 @@
 import express, { Application } from 'express';
-import baseRoutes from './routes/baseRoutes'
+import mongoose from 'mongoose';
+import baseRoutes from './routes/baseRoutes';
+
+import { dbUri } from './utils/constant';
 
 const app: Application = express();
 
@@ -7,5 +10,18 @@ app.use('/', baseRoutes);
 
 app.listen(5000, () => {
     console.log(`Server is running on http://localhost:5000`);
+    try {
+        connectToDb();
+    } catch(err) {
+        console.log(err);
+    }
 });
 
+async function connectToDb() {
+    try {
+        await mongoose.connect(dbUri);
+        console.log("Connect to MongoDB");
+    } catch(err) {
+        console.error('Error: ' + err);
+    }
+};
