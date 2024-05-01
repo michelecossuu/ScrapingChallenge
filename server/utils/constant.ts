79886bd563ad4e9b61bc9444f559d6cc79f932a7
mcossu <mcossu@intesigroup.com>
2024-05-01 11:55:41 +0200

    Add db uri to constants

--- server/utils/constant.ts
+++ server/utils/constant.ts
@@ -3,3 +3,6 @@ const targetUrl = `https://wms.cartografia.agenziaentrate.gov.it/`;
 export const getAllRegioniQuery = targetUrl + `inspire/ajax/ajax.php?op=getRegioni`;
 export const getAllProvinceFromRegioneQuery = (region: string) => targetUrl + `inspire/ajax/ajax.php?op=getProvince&reg=${region}`;
 export const getAllComuniFromProvinciaQuery = (province: string) => targetUrl + `inspire/ajax/ajax.php?op=getComuniSez&prov=${province}`;
+
+/** url db mongo */
+export const dbUri = 'mongodb+srv://dev:1234@mydb.bpxobfe.mongodb.net/?retryWrites=true&w=majority&appName=mydb';
